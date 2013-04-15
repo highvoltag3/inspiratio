ba1a6d2ebf454c92abdd6428d0c225079f647527
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-04-15 19:08:46 +0200

    add tagging to Idea model (work in progress)

--- ror/inspiratio/app/models/idea.rb
+++ ror/inspiratio/app/models/idea.rb
@@ -2,7 +2,9 @@ class Idea < ActiveRecord::Base
 	belongs_to :user
   has_many :ideas
 
-	attr_accessible :user_id, :title, :description
+	attr_accessible :user_id, :title, :description, :tag_list
+
+  acts_as_taggable # add tags
 
   validates :user_id, presence: true
   validates :title, presence: true
