63d08d3f95aa3d3bb2faaa2c2dc4a37c6ea79cd3
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-04-15 17:52:57 +0200

    add paperclip to Upload

--- ror/inspiratio/app/models/upload.rb
+++ ror/inspiratio/app/models/upload.rb
@@ -1,7 +1,10 @@
 class Upload < ActiveRecord::Base
   belongs_to :idea
 
-  attr_accessible :idea_id
+  attr_accessible :idea_id, :image
+
+  has_attached_file :image
+    # styles: { medium: "300x300>", thumb: "100x100>" },
 
   validates user_id: presence: true
 
