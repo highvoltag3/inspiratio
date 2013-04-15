ed7702f31359da77b799bbc106471d83dd76ac91
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-04-15 19:05:26 +0200

    setup acts_as_taggable migration

--- ror/inspiratio/db/schema.rb
+++ ror/inspiratio/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415154521) do
+ActiveRecord::Schema.define(:version => 20130415170345) do
 
   create_table "ideas", :force => true do |t|
     t.integer  "user_id"
@@ -22,6 +22,23 @@ ActiveRecord::Schema.define(:version => 20130415154521) do
   end
 
   add_index "ideas", ["user_id"], :name => "index_ideas_on_user_id"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
   create_table "uploads", :force => true do |t|
     t.integer  "idea_id"
