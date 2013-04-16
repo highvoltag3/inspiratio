9af1d232b9074e37de7b252fba929a517b137f23
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-04-16 18:14:12 +0200

    fix Idea#current_image method calls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,4 @@ class HomeController < ApplicationController
     @idea = Idea.where(id: params[:id]).first
   end
 
-  #this action will let the users download the files (after a simple authorization check)
-  # TODO: this is wrong, check a better way to do this
-  #
-  def get
-    if idea = Idea.where(id: params[:id]).first
-      send_file idea.current_image.path, type: idea.current_image_content_type
-    end
-  end
-
 end
