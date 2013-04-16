a20419b9a96c49733e66267bc928741b6911aaf9
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-04-16 14:02:16 +0200

    complete upload to s3 and image retrieval

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,13 +1,25 @@
 class Idea < ActiveRecord::Base
 	belongs_to :user
-  has_many :ideas
+  has_many :uploads, dependent: :destroy
 
-	attr_accessible :user_id, :title, :description, :tag_list
+	attr_accessible :user_id, :title, :description, :tag_list, :uploads_attributes
 
   acts_as_taggable # add tags
+
+  accepts_nested_attributes_for :uploads, allow_destroy: true
 
   validates :user_id, presence: true
   validates :title, presence: true
   validates :description, presence: true
+
+  def current_image
+    self.uploads.last
+  end
+
+  def old_images
+    images = self.uploads
+    images.pop
+    images
+  end
 
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,9 +3,18 @@ class Upload < ActiveRecord::Base
 
   attr_accessible :idea_id, :image
 
-  has_attached_file :image
-    # styles: { medium: "300x300>", thumb: "100x100>" },
+  has_attached_file :image,
+    storage: :s3,
+    s3_credentials: {
+      access_key_id: APP_CONF[:aws][:key],
+      secret_access_key: APP_CONF[:aws][:secret]
+    },
+    bucket: APP_CONF[:aws][:bucket],
+    s3_permissions: :public_read,
+    url: ':s3_domain_url',
+    path: ":class/:id/:filename"
 
-  validates :user_id, presence: true
+  # validates :idea_id, presence: true
+  validates :image, presence: true
 
 end
