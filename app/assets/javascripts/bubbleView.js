75c238e71836bf7f4cb7e56d0058ebe3c4610c77
Dario Novoa <darionovoa@ideartte.com>
2013-04-22 20:15:19 -0700

    Get all keywords not just 3
    we want to get all tags not just 3 of them.

--- app/assets/javascripts/bubbleView.js
+++ app/assets/javascripts/bubbleView.js
@@ -14,24 +14,25 @@ $(document).ready(function() {
 
             keywords = $('.hiddentags').val();
             var searchKeywords = [];
-            searchKeywords = keywords.split(' ', 3);
+            searchKeywords = keywords.split(' ');
 
             var wordsresults = [];
 
             var newdata = {};
-           
+            
+            var ran = false;
+
             $.each(searchKeywords, function(index, value) {
               wordtolookup = value;
-
+              
               var root = $('section h2').get(0).mynode = $('#bubbleView').addRootNode($('.inv_idea_title').text(),{});
-      
+              
               if ( typeof(parentnode) === 'undefined') {
-                parentnode=root; 
+                parentnode = root; 
               } else {
-                parentnode=parentnode.mynode;
+                parentnode = parentnode.mynode;
               }
               this.mynode = $('#bubbleView').addNode(parentnode, wordtolookup, {});
-              
 
               $.ajax({
                 url: 'http://api.wordnik.com/v4/word.json/' + wordtolookup + '/relatedWords?api_key=e22c18406c64c389fe4000964950f380a3474965ab180be08&useCanonical=true&relationshipTypes=same-context&limitPerRelationshipType=10',
@@ -54,8 +55,6 @@ $(document).ready(function() {
                 
 
   function addNodeWordnik(wordsresults, wordtolookup) {
-      
-
       
       //populate second level sub-childs
       $.each(wordsresults, function(index, value) {
