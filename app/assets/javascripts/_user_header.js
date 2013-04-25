a426aba4900b5bb1de6852243d09ea6336891271
Dario Novoa <darionovoa@ideartte.com>
2013-04-24 20:06:09 -0700

    Better handling of the notifications.
    Including cookie storing.

--- app/assets/javascripts/_user_header.js
+++ app/assets/javascripts/_user_header.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
         $('#notificationsWrapper').bind('mouseleave', function() {
             hideTimer = setTimeout(function() {
                 $('#notificationsWrapper').fadeOut('normal');
+                $('.count').html('0');
             }, 1000);
         });
 
@@ -23,6 +24,12 @@ $(document).ready(function() {
     }
     $(this).css('background', '#999');
     e.stopPropagation();
+  });
+
+  $('.notification .close').click(function (e) {
+      //user has dismissed the notification.
+      createCookie('notsViewed','yes',30);
+      $('.count').html('0');
   });
 
 }); /* end on ready function */
@@ -40,13 +47,40 @@ function runFunctionIfEnter(e, fnName) {
   }
 }
 
+var avatar = $('.useravatar img').attr('src');
 function makeComment() {
   var inputFiled = $('input.reply');
   var commentText = inputFiled.val();
-  $('.triangle_top_notInput').before('<p class="response"><img class="fl" src="imgs/avatar.png" alt="avatar" width="40" height="40">' + commentText + '</p>');
+  $('.triangle_top_notInput').before('<p class="well response"><img class="fl" src="' + avatar + '" alt="avatar" width="40" height="40">' + commentText + '</p>');
   inputFiled.val('');
+  createCookie('notsViewed','yes',30);
 }
 
 jQuery(document).keypress(function(e) {
   runFunctionIfEnter(e, 'makeComment');
 });
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
+}
